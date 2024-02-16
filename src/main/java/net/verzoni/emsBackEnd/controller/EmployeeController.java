6c3c26af9565a9c70135d00bdfb1e33aef7895dd
Massimo <verzoni.massimo@gmail.com>
2024-02-16 19:08:23 +0100

    emsBackEnd first commit

--- src/main/java/net/verzoni/emsBackEnd/controller/EmployeeController.java
+++ src/main/java/net/verzoni/emsBackEnd/controller/EmployeeController.java
@@ -3,12 +3,10 @@ package net.verzoni.emsBackEnd.controller;
 import lombok.AllArgsConstructor;
 import net.verzoni.emsBackEnd.dto.EmployeeDto;
 import net.verzoni.emsBackEnd.service.EmployeeService;
-import org.springframework.data.repository.query.Param;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.sql.SQLException;
 import java.util.List;
 
 @AllArgsConstructor
